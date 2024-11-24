3349f7cffc8c4ba899185b1510f45e9cd8c43435
jakublo898 <jakublondon@unomaha.edu>
2024-11-24 17:46:58 -0600

    Changes for resubmission

--- television.py
+++ television.py
@@ -1,66 +1,88 @@
 class Television:
-    #Create class variables
-    min_volume = 0
-    max_volume = 2
-    min_channel = 0
-    max_channel = 3
-    def __init__(self):
-        #initialized variables
-        self._private_status = False
-        self._private_muted = False
-        self._private_volume = self.min_volume
-        self._private_channel = self.min_channel
+    """
+    A class to represent a Television with basic functionalities like power, mute,
+    channel change, and volume control.
+    """
 
-    def power(self):
-        #Turns power on and off
-        if self._private_status is False:
-            self._private_status = True
-        else:
-            self._private_status = False
+    # Class variables
+    min_volume: int = 0
+    max_volume: int = 2
+    min_channel: int = 0
+    max_channel: int = 3
 
-    def mute(self):
-        #Checks if power is on and then mutes/unmutes
-        if self._private_status is True:
-            if self._private_muted is False:
-                self._private_muted = True
+    def __init__(television) -> None:
+        """
+        Initializes the Television instance with default settings:
+        - Power is off
+        - Muted is off
+        - Volume is set to the minimum
+        - Channel is set to the minimum
+        """
+        television._private_status: bool = False
+        television._private_muted: bool = False
+        television._private_volume: int = television.min_volume
+        television._private_channel: int = television.min_channel
+
+    def power(television) -> None:
+        """
+        Toggles power
+        """
+        television._private_status = not television._private_status
+
+    def mute(television) -> None:
+        """
+        Toggles mute
+        """
+        if television._private_status:
+            television._private_muted = not television._private_muted
+
+    def channel_up(television) -> None:
+        """
+        Increases the channel by one. If the channel is at the maximum, it loops back to the minimum.
+        Only works if the Television is powered on.
+        """
+        if television._private_status:
+            if television._private_channel < television.max_channel:
+                television._private_channel += 1
             else:
-                self._private_muted = False
+                television._private_channel = television.min_channel
 
-    def channel_up(self):
-        #Checks if power is on and moves the channel up and goes back to min if at max
-        if self._private_status is True:
-            if self._private_channel < self.max_channel:
-                self._private_channel += 1
-            elif self._private_channel == self.max_channel:
-                self._private_channel = self.min_channel
-
-    def channel_down(self):
-        #Takes channel down and if at min moves to max
-        if self._private_status is True:
+    def channel_down(self) -> None:
+        """
+        Decreases the channel by one. If the channel is at the minimum, it loops back to the maximum.
+        Only works if the Television is powered on.
+        """
+        if self._private_status:
             if self._private_channel > self.min_channel:
                 self._private_channel -= 1
-            elif self._private_channel == self.min_channel:
+            else:
                 self._private_channel = self.max_channel
 
-    def volume_up(self):
-        #Makes volume go up to max and no more
-        if self._private_status is True:
+    def volume_up(self) -> None:
+        """
+        Increases the volume by one up to the maximum. Unmutes the Television if it is muted.
+        Only works if the Television is powered on.
+        """
+        if self._private_status:
             self._private_muted = False
             if self._private_volume < self.max_volume:
                 self._private_volume += 1
 
-    def volume_down(self):
-        #makes volume go down to min and no less
-        if self._private_status is True:
+    def volume_down(self) -> None:
+        """
+        Decreases the volume by one down to the minimum. Unmutes the Television if it is muted.
+        Only works if the Television is powered on.
+        """
+        if self._private_status:
             self._private_muted = False
             if self._private_volume > self.min_volume:
                 self._private_volume -= 1
 
-    def __str__(self):
-        #Checks if the tv is muted and outputs a 0 for the volume otherwise it gives the regular volume
-        if self._private_muted is True:
-            volume_str = 0
-        else:
-            volume_str = self._private_volume
-        return f'Power = [{self._private_status}], Channel = [{self._private_channel}], Volume = [{volume_str}]'
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the Television's current state.
 
+        If the Television is muted, the volume is displayed as 0.
+        """
+        volume_str: int = 0 if self._private_muted else self._private_volume
+        return f'Power = {self._private_status}, Channel = {self._private_channel}, Volume = {volume_str}'
--- test_television.py
+++ test_television.py
@@ -4,7 +4,7 @@ from television import *
 
 def test_initial_state():
     tv = Television()
-    assert str(tv) == "Power = [False], Channel = [0], Volume = [0]"
+    assert str(tv) == "Power = False, Channel = 0, Volume = 0"
 
 
 def test_power_toggle():
@@ -18,10 +18,10 @@ def test_volume_up():
     tv = Television()
     tv.power()
     tv.volume_up()
-    assert str(tv) == "Power = [True], Channel = [0], Volume = [1]"
+    assert str(tv) == "Power = True, Channel = 0, Volume = 1"
     tv.volume_up()
     tv.volume_up()
-    assert str(tv) == "Power = [True], Channel = [0], Volume = [2]"
+    assert str(tv) == "Power = True, Channel = 0, Volume = 2"
     # Max volume
 
 def test_volume_down():
@@ -29,11 +29,11 @@ def test_volume_down():
     tv.power()
     tv.volume_up()
     tv.volume_down()
-    assert str(tv) == "Power = [True], Channel = [0], Volume = [0]"
+    assert str(tv) == "Power = True, Channel = 0, Volume = 0"
     tv.power()
     tv.volume_up()
     tv.volume_up()
-    assert str(tv) == "Power = [False], Channel = [0], Volume = [0]"
+    assert str(tv) == "Power = False, Channel = 0, Volume = 0"
     #Checks for volume not to be changed since power is off
 
 def test_mute():
@@ -42,24 +42,24 @@ def test_mute():
     tv.volume_up()
     tv.volume_up()
     tv.mute()
-    assert str(tv) == "Power = [True], Channel = [0], Volume = [0]"
+    assert str(tv) == "Power = True, Channel = 0, Volume = 0"
     tv.mute()
-    assert str(tv) == "Power = [True], Channel = [0], Volume = [2]"
+    assert str(tv) == "Power = True, Channel = 0, Volume = 2"
     tv.mute()
     tv.volume_down()
     #Unmutes tv and reduces volume
-    assert str(tv) == "Power = [True], Channel = [0], Volume = [1]"
+    assert str(tv) == "Power = True, Channel = 0, Volume = 1"
 
 def test_channel_up_and_down():
     tv = Television()
     tv.power()
     tv.channel_up()
-    assert str(tv) == "Power = [True], Channel = [1], Volume = [0]"
+    assert str(tv) == "Power = True, Channel = 1, Volume = 0"
     tv.channel_up()
     tv.channel_up()
     tv.channel_up()
     # Wraps to the min channel
-    assert str(tv) == "Power = [True], Channel = [0], Volume = [0]"
+    assert str(tv) == "Power = True, Channel = 0, Volume = 0"
     tv.channel_down()
-    assert str(tv) == "Power = [True], Channel = [3], Volume = [0]"
+    assert str(tv) == "Power = True, Channel = 3, Volume = 0"
     # Wraps to the max channel
